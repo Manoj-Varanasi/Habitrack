c5c37019bca0b2e6e8dc1419da28627f8939c1b0
Manoj-Varanasi <manojvaranasi13@gmail.com>
2026-06-19 14:42:19 +0530

    added addHabit function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 #include "sqlite3.h"
 using namespace std;
 
+void addHabit(sqlite3* db, const string& habitName) {
+    string sql = "insert into habits (name) values (?);";
+    sqlite3_stmt* stmt = nullptr;
+    int rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
+    if (rc != SQLITE_OK) {
+        cout << "Failed to prepare message: " << sqlite3_errmsg(db) << endl;
+        return;
+    }
+    sqlite3_bind_text(stmt, 1, habitName.c_str(), -1, SQLITE_TRANSIENT);
+    rc = sqlite3_step(stmt);
+    if (rc == SQLITE_DONE) {
+        cout << "Habit added successfully!" << endl;
+    } else {
+        cout << "Failed to add habit: " << sqlite3_errmsg(db) << endl;
+    }
+    sqlite3_finalize(stmt);
+}
+
 int main() {
     sqlite3* db = nullptr;
     int rc = sqlite3_open("habits.db", &db);
@@ -18,12 +36,14 @@ int main() {
     )";
     char* errMsg = nullptr;
     rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-    if(rc!=SQLITE_OK){
-        cout<<"Error in SQL : "<<errMsg<<endl;
+    if (rc != SQLITE_OK) {
+        cout << "Error in SQL : " << errMsg << endl;
         sqlite3_free(errMsg);
         sqlite3_close(db);
         return 1;
     }
-    cout<<"Table created successfully"<<endl;
+    cout << "Table created successfully" << endl;
+    addHabit(db, "Reading Book");
+    addHabit(db, "Jogging");
     sqlite3_close(db);
 }
